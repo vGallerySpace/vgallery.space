820ef1f2de951a6d51f6752e88aceef30a83a876
dataspace <dataspace@admin@vgallery.space>
2026-09-08 06:50:42 -0500

    Proofread vgalleryspace-ebay.html, encode ampersand in medium line, and left-justify hero image

---
+++ serve.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+import os
+
+class RobustHandler(http.server.SimpleHTTPRequestHandler):
+    def copyfile(self, source, outputfile):
+        try:
+            super().copyfile(source, outputfile)
+        except (ConnectionResetError, BrokenPipeError, ConnectionAbortedError):
+            pass
+
+class ThreadingServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
+    daemon_threads = True
+    allow_reuse_address = True
+
+if __name__ == '__main__':
+    os.chdir('/home/dataspace/vgallery-rebuild')
+    server = ThreadingServer(('0.0.0.0', 8080), RobustHandler)
+    print('Serving at port 8080...')
+    server.serve_forever()
